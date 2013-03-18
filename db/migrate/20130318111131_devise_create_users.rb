bb6eccff8ce4f70211008a08bb5d0aa7d7d0f91f
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-03-18 15:23:15 +0100

    refine User model and start migration

--- db/migrate/20130318111131_devise_create_users.rb
+++ db/migrate/20130318111131_devise_create_users.rb
@@ -1,13 +1,14 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.string :username, :null => false, :default => ""
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -38,7 +39,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                :unique => true
-    add_index :users, :reset_password_token, :unique => true
+    add_index :users, :username,             :unique => true
+    # add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
