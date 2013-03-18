5e05826dfc59436015e3fdb73896635f2bbdd9d0
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-03-18 13:11:13 +0100

    improve User validations (unique username, case insensitive)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,9 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
 
-  validates :username, :presence => true, :uniqueness => true
+  validates :username,
+    :presence => true,
+    :uniqueness => {:case_sensitive => false},
+    :format => {:with => /\w+/}
+
 end
