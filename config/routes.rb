135ad0b335dff96ac57f9438fc58afad372ee1b3
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-03-18 12:02:22 +0100

    add dummy root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Bookmarks::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+
+  # dummy route, not yet existent
+  root to: "home#index"
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
