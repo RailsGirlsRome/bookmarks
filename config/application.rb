2df89cdd9c66eef21507864ba5fa4b69acfe6cbb
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-03-20 11:39:55 +0100

    prevent environment to be loaded during assets precompile

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Bookmarks
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # prevents the Rails environment to be loaded when the assets:precompile task is executed
+    config.assets.initialize_on_precompile = false
   end
 end
